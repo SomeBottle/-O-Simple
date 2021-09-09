f0f0e11f117a01057fd2166e8d08ea91e691b042
SomeBottle <somebottle@outlook.com>
2021-09-09 20:00:27 +0800

    Update to -O-4.2.0

--- addition.js
+++ addition.js
@@ -1,5 +1,27 @@
+/*The underline of navigation*/
 B.nav({
     activeclass: 'current',
     navclass: 'nvbtn'
 });
 B.navcheck();
+/*The function for toggling the lazyload*/
+var onlazy = true;
+function togglelazy(element) {
+    if (onlazy) {
+        onlazy = false;
+        element.innerHTML = "我要加载图片！";
+        B.lazy(false);
+    } else {
+        onlazy = true;
+        element.innerHTML = "不要加载图片";
+        B.lazy(true);
+    }
+}
+/*The listener of 'backtotop' button*/
+window.addEventListener("scroll", () => {
+    if (document.body.scrollTop <= 700) {
+        SC("backtop").style.opacity = 0;
+    } else {
+        SC("backtop").style.opacity = 1;
+    }
+}, false);
